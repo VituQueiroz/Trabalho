cf4c79019bb83d94309626a6702b3c6ea81c67d6
VituQueiroz <vitinho04112005@gmail.com>
2024-09-02 23:33:05 -0300

    02.09.24-23:32

--- C9.c
+++ C9.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <locale.h>
 
+int main() {
+    setlocale(LC_ALL,"");
+    int i, filhos, salario;
+
+    for (i = 1; i <= 5; i++) {
+        printf("Digite o salário da %d° pessoa: ", i);
+        scanf("%d", &salario);
+        printf("Digite a quantidade de filhos da %d° pessoa: ", i);
+        scanf("%d", &filhos);
+        salario++;
+        filhos++;
+    }
+        printf("A média dos salários é: %d\n", salario / 5);
+        printf("A média de filhos é: %d\n", filhos / 5);
+
+    return 0;
+}
